91cb82889cca0370aa91bea3fed195bfad1130a9
chirptotheeast <alyssa.easterly@yahoo.com>
2021-06-20 21:45:09 -0400

    added Header and Search Input js and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { PureComponent } from "react";
+import Header from "./Header";
+import SearchInput from "./SearchInput";
+import EmojiResults from "./EmojiResults";
+import filteredEmoji from "./filterEmoji"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends PureComponent(){
+  constructor(props){
+    super(props);
+    this.state = {
+      filteredEmoji: filteredEmoji("", 20)
+    };
+
+    handleSearchChange = event => {
+      this.setState({
+        filteredEmoji: filteredEmoji(event.target.value, 20)
+      });
+    }
+  }
+  render(){
+    return <div className="App">
+      <Header />
+      <SearchInput textChange={this.handleSearchChange}/>
+      <EmojiResults emojiData={this.state.filteredEmoji}/>
+    </div>;
+  }
+  
 }
 
-export default App;
+
